53a62daf87abfefe5dc261251431197aad9048a7
Matt Condon (shrugs) <mattgcondon@gmail.com>
2021-05-13 10:47:10 -0400

    dev: set up husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite';
+import ViteFonts from 'vite-plugin-fonts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    ViteFonts({
+      // google: {
+      //   families: ['Source Sans Pro'],
+      // },
+    }),
+  ],
 });
